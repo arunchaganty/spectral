14969f51c78648b92086ae4dc14892d4a86cc206
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2012-10-19 22:35:51 -0700

    Added a Munk-res maximal weighted matching algorithm and computing the closest permutation between two matrices

--- src/spectral/linalg.py
+++ src/spectral/linalg.py
@@ -3,8 +3,10 @@ Linear algebra methods for spectral learning
 """
 
 import scipy as sc 
-from scipy import array
-from scipy.linalg import svd, svdvals
+from scipy import array, diag
+from scipy.linalg import svd, svdvals, norm
+
+from munkres import Munkres
 
 def apply_permutation( perm, lst ):
     """Apply a permutation to a list"""
@@ -56,10 +58,15 @@ def canonicalise( x ):
     perm = canonical_permutation( x )
     return apply_matrix_permutation( perm, x )
 
-def svdk( x, k ):
+def svdk( X, k ):
     """Top-k SVD decomposition"""
-    U, D, Vt = svd( x, full_matrices=False )
+    U, D, Vt = svd( X, full_matrices=False )
     return U[:, :k], D[:k], Vt[:k, :]
+
+def approxk( X, k ):
+    """Best k rank approximation of X"""
+    U, D, Vt = svdk( X, k )
+    return U.dot( diag( D ) ).dot( Vt )
 
 def mrank( x, eps=1e-12 ):
     """Matrix rank"""
@@ -80,6 +87,65 @@ def eigengap( x, k = None ):
     """Minimum difference in eigenvalues"""
     # Get the eigenvalues
     s = svdvals( x )
+    if k is not None:
+        s = s[:k]
 
     return sc.diff( s ).min() / s[0]
+
+def closest_permuted_vector( a, b ):
+    """Find a permutation of b that matches a most closely (i.e. min |A
+    - B|_2)"""
+
+    # The elements of a and b form a weighted bipartite graph. We need
+    # to find their minimal matching.
+    assert( a.shape == b.shape )
+    n, = a.shape
+
+    W = sc.zeros( (n, n) )
+    for i in xrange( n ):
+        for j in xrange( n ):
+            W[i, j] = (a[i] - b[j])**2
+
+    m = Munkres()
+    matching = m.compute( W )
+    matching.sort()
+    _, bi = zip(*matching)
+
+    return bi
+
+def closest_permuted_matrix( A, B ):
+    """Find a _row_ permutation of B that matches A most closely (i.e. min |A
+    - B|_F)"""
+
+    # The rows of A and B form a weighted bipartite graph. The weights
+    # are computed using the vector_matching algorithm.
+    # We need to find their minimal matching.
+    assert( A.shape == B.shape )
+
+    n, _ = A.shape
+    m = Munkres()
+
+    # Create the weight matrix
+    W = sc.zeros( (n, n) )
+    for i in xrange( n ):
+        for j in xrange( n ):
+            # Best matching between A and B
+            W[i, j] = norm(A[i] - B[j])
+        
+    matching = m.compute( W )
+    matching.sort()
+    _, rowp = zip(*matching)
+    rowp = array( rowp )
+    # Permute the rows of B according to Bi
+    B_ = B[ rowp ]
+
+    return B_
+
+def test_closest_permuted_matrix():
+    A = array([[0, 1, 2],[2,3,4],[4,5,6]])
+    B = array([[2.3, 3.1, 4.1], [4.1,5.2,5.9], [0.1,1.3,1.9]])
+    Bo = array([[0.1, 1.3, 1.9], [2.3, 3.1, 4.1], [4.1, 5.2, 5.9]])
+
+    B_ = closest_permuted_matrix( A, B )
+    assert( sc.allclose( B_, Bo ) )
 
