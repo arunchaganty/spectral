acd6430490b5f34c2079ba16f2e45a244b0b0220
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2012-10-26 00:48:15 -0700

    Fixed progress bar

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -18,31 +18,31 @@ class ProgressBar:
 
     def start(self, total_iterations):
         """Set up a scaling factor for total iterations"""
-        # setup toolbar
-        self.total = total_iterations
         sys.stdout.write("[%s]" % (" " * self.width))
         sys.stdout.flush()
         # return to start of line, after '['
         sys.stdout.write("\b" * (self.width+1)) 
 
-        self.state = 0
+        self.state, self.total = 0, total_iterations
 
     def update(self, n):
         """Update the ticker"""
-        state_ = (self.width * n)/self.total
+        state_ = int(self.width*n)/int(self.total)
         if state_ == self.state:
             pass
         elif self.state < self.width and state_ > self.state:
             # update the bar
-            sys.stdout.write("-")
+            for i in xrange( state_ - self.state ):
+                sys.stdout.write("-")
             sys.stdout.flush()
-            self.state += 1
+            self.state = state_
 
     def stop(self):
         """End the ticker"""
+        for i in xrange( self.width - self.state ):
+            sys.stdout.write("-")
         sys.stdout.write("\n")
-        self.state = 0
-        self.total = 0
+        self.state, self.total = 0, 0
 
 def chunked_update( out, n, blocksize, fn, *params ):
     """Run @fn to produce samples @blocksize at a time. This
@@ -54,14 +54,15 @@ def chunked_update( out, n, blocksize, fn, *params ):
     """
 
     pbar = ProgressBar()
-    pbar.start( n )
 
     # Draw "blocksize" number of samples for 1 less than required blocks
     blocks = int( sc.floor( float(n) / blocksize ) )
+
+    pbar.start( blocks )
     for block in xrange( blocks ):
         out[block * blocksize : (block+1)*blocksize ] = fn(
                 *params, size=blocksize ) 
-        pbar.update( block ** blocksize )
+        pbar.update( block )
 
     # Draw the remaining number of samples.
     out[ blocks * blocksize : ] = fn( *params, size = n - blocks
