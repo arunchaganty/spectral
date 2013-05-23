cfb81c5b5b3f7bd84b69378f41aab99aaa4b3d09
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2013-05-22 17:26:48 -0700

    Actual marginal computaitons

--- java/src/learning/spectral/applications/HiddenMarkovModel.java
+++ java/src/learning/spectral/applications/HiddenMarkovModel.java
@@ -37,6 +37,12 @@ import java.lang.ClassNotFoundException;
  */
 public class HiddenMarkovModel implements Runnable {
 
+  /**
+   * Runs tensor factorization on the moments of the data and returns
+   * (\pi, T, O).
+   * Note that unless the moments are calculated using only the first 3
+   * words, $\pi$ and $T$ are not exact.
+   */
   public Triplet<SimpleMatrix, SimpleMatrix, SimpleMatrix>
       run( int K, 
           SimpleMatrix M12, SimpleMatrix M13, SimpleMatrix M23, FullTensor M123 ) {
@@ -44,9 +50,19 @@ public class HiddenMarkovModel implements Runnable {
     TensorMethod algo = new TensorMethod();
     Quartet<SimpleMatrix, SimpleMatrix, SimpleMatrix, SimpleMatrix> params = algo.recoverParameters( K, M12, M13, M23, M123 );
 
+    // T \pi
+    SimpleMatrix Tpi = params.getValue0();
+    // O
+    SimpleMatrix O = params.getValue2();
+    // O T
+    SimpleMatrix OT = params.getValue3();
+
+    SimpleMatrix T = O.pseudoInverse().mult(OT);
+    SimpleMatrix pi = T.invert().mult(Tpi);
+
     // Return emission and transition probabilities.
 
-    return null;
+    return new Triplet<>( pi, T, O );
   }
 
   public void run() {}
