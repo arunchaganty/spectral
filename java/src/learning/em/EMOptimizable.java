7f4479c019f5a34541935f5b7c3deb89fdcfaf43
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2013-05-30 12:18:51 -0700

    Implemented HMMs with EM (Baum-Welch) training.

---
+++ java/src/learning/em/EMOptimizable.java
@@ -0,0 +1,41 @@
+/**
+ * learning.spectral
+ * Arun Chaganty <chaganty@stanford.edu
+ *
+ */
+package learning.em;
+
+import learning.Misc;
+import learning.linalg.*;
+import learning.data.*;
+import learning.spectral.TensorMethod;
+import learning.models.loglinear.*;
+import learning.models.loglinear.Models.HiddenMarkovModel;
+
+import org.ejml.simple.SimpleMatrix;
+import org.javatuples.Quartet;
+
+import fig.basic.*;
+import fig.exec.*;
+import fig.prob.*;
+import fig.record.*;
+import static fig.basic.LogInfo.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Learns EM via optimization.
+ * For likelihoods like \theta^T E_q
+ */
+public interface EMOptimizable {
+
+  public int numFeatures();
+  // L = \theta^T \E_q, gradient = \E_q
+  public double compute(double[] params, int[][] X, double[] gradient);
+
+}
+
+
+
+
